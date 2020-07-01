289bef8fe190f6232c7140bebcc9ad2f25dff352
al1729 <andyliu1708@gmail.com>
2020-07-01 17:09:07 -0500

    added txt file

--- gnews_scrape.py
+++ gnews_scrape.py
@@ -6,13 +6,18 @@ import time
 linklist = []
 
 while True:
-    html_page = urllib.request.urlopen("https://news.google.com/topics/CAAqBwgKMOfAkAsw0bukAw?hl=en-US&gl=US&ceid=US%3Aen")
-    soup = BeautifulSoup(html_page, 'lxml')
-    links = soup.findAll('a', {'class': ['VDXfz']})
+    with open("./links.txt", "a") as f:
+        html_page = urllib.request.urlopen("https://news.google.com/topics/CAAqBwgKMOfAkAsw0bukAw?hl=en-US&gl=US&ceid=US%3Aen")
+        soup = BeautifulSoup(html_page, 'lxml')
+        links = soup.findAll('a', {'class': ['VDXfz']})
 
-    for link in links:
-        if link['href'] not in linklist:
-            linklist.append(link['href'])
+        for link in links:
+            if link['href'] not in linklist:
+                linklist.append(link['href'])
+                f.write(link['href'])
+                f.write('\n')
 
-    print(len(linklist))
-    time.sleep(20)
+        print(len(linklist))
+        f.close()
+
+    time.sleep(60)
