f85ba6be546a96d67ee2546c3e62937c63009c3e
al1729 <andyliu1708@gmail.com>
2020-07-01 16:56:05 -0500

    update

--- gnews_scrape.py
+++ gnews_scrape.py
@@ -1,12 +1,18 @@
 from bs4 import BeautifulSoup
 import urllib
 import re
+import time
 
-html_page = urllib.request.urlopen("https://news.google.com/topics/CAAqBwgKMOfAkAsw0bukAw?hl=en-US&gl=US&ceid=US%3Aen")
-soup = BeautifulSoup(html_page, 'lxml')
-links = soup.findAll('a', {'class': ['VDXfz']})
+linklist = []
 
-'''print(len(links))
+while True:
+    html_page = urllib.request.urlopen("https://news.google.com/topics/CAAqBwgKMOfAkAsw0bukAw?hl=en-US&gl=US&ceid=US%3Aen")
+    soup = BeautifulSoup(html_page, 'lxml')
+    links = soup.findAll('a', {'class': ['VDXfz']})
 
-for link in links:
-    print(link['href'])'''
+    for link in links:
+        if link['href'] not in linklist:
+            linklist.append(link['href'])
+
+    print(len(linklist))
+    time.sleep(20)
