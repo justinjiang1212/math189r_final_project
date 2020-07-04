b657b43351aea67340dbf22ff1327c354fe824d3
justinjiang1212 <jbjiang@hmc.edu>
2020-07-04 14:34:49 -0700

    reduce for loops

--- google_news.py
+++ google_news.py
@@ -10,20 +10,23 @@ urls = []
 for link in links:
   urls.append("https://news.google.com" + link[1:-2])
 
+articles = []
 
 for i in range(0, len(urls)):
   r = requests.get(urls[i])
   print(str(i), " out of ", str(len(urls)), " done redirecting")
-  urls[i] = r.url
+  #urls[i] = r.url
+  article = NewsPlease.from_url(r)
+  articles.append((article.title, r, article.maintext))
+  print(str(i), " out of ", str(len(urls)), " scraped")
 
-articles = []
-counter = 0
 
-for url in urls:
-  article = NewsPlease.from_url(url)
-  articles.append((article.title, url, article.maintext))
-  print(str(counter), " out of ", str(len(urls)), " done redirecting")
-  counter += 1
+
+#for url in urls:
+#  article = NewsPlease.from_url(url)
+#  articles.append((article.title, url, article.maintext))
+#  print(str(counter), " out of ", str(len(urls)), " done redirecting")
+#  counter += 1
 
 with open("./articles.txt", "a") as f:
   for article in articles:
