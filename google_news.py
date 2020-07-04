d26b55150daf636aabf9185cb4d309c9563a6899
justinjiang1212 <jbjiang@hmc.edu>
2020-07-04 13:52:39 -0700

    add article scraping

--- google_news.py
+++ google_news.py
@@ -1,11 +1,33 @@
 from newsplease import NewsPlease
 import requests
 
+f = open("links.txt", 'r')
+links = []
+for x in f:
+  links.append(x)
 
-url = "https://news.google.com/articles/CAIiEJEOihpTd7_0e6BTqCjyKowqGQgEKhAIACoHCAowh___CjDHqosDMOj-4gU?hl=en-US&gl=US&ceid=US%3Aen"
+urls = []
+for link in links:
+  urls.append("https://news.google.com" + link[1:-2])
 
-r = requests.get(url) 
 
-article = NewsPlease.from_url(r.url)
+for i in range(0, len(urls)):
+  r = requests.get(urls[i])
+  print(str(i), " out of ", str(len(urls)), " done redirecting")
+  urls[i] = r.url
 
-print(article.maintext)
+articles = []
+counter = 0
+
+for url in urls:
+  article = NewsPlease.from_url(url)
+  articles.append((article.title, article.maintext))
+  print(str(counter), " out of ", str(len(urls)), " done redirecting")
+  counter += 1
+
+with open("./articles.txt", "a") as f:
+  for article in articles:
+    f.write(article[0])
+    f.write("|")
+    f.write(article[1])
+    f.write('\n')
