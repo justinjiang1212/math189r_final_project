d5b0547e0bfb09df804dbb98a2b9ae4031188ab3
justinjiang1212 <jbjiang@hmc.edu>
2020-07-05 13:39:50 -0700

    split links for news please input

--- link_splitter.py
+++ link_splitter.py
@@ -12,22 +12,24 @@ while counter <= (len(links) // 100 -1):
   file_names.append(file_name)
   if os.path.exists(file_name):
     os.remove(file_name)
-    print(file_name + " removed")
+
   with open(file_name, "w") as f:
     print(counter * 100, (counter * 100) + 100)
     for i in range(counter * 100, (counter * 100) + 100):
       f.write(links[i])
       f.write("\n")
+  f.close
   counter += 1
+  
 
 file_name = "links" + str(counter) + ".txt"
 file_names.append(file_name)
-with open(file_name, "a") as f:
+with open(file_name, "w") as f:
   for i in range((counter -1) * 100, len(links)):
     f.write(links[i])
     f.write("\n")
-
 f.close
+
 
 #for file in file_names:
 #  command = "python3 google_news.py " + file
