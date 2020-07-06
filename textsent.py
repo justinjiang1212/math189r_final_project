803e9d20b2f9b606df806f2e64a8355b9bc1379c
al1729 <andyliu1708@gmail.com>
2020-07-06 12:29:32 -0500

    new textsent

--- textsent.py
+++ textsent.py
@@ -1,11 +1,21 @@
 import csv
 import pandas as pd
 import nltk
-nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+truncated = pd.read_csv("/Users/andyliu/Downloads/new_truncated.csv")
+
+nltk.download('vader_lexicon')
 sid = SentimentIntensityAnalyzer()
 
-truncated = pd.read_csv('/Users/andyliu/Downloads/truncated.csv')
+def getText(index):
+    #get the article text from the other spreadsheet (from the index)
+    row = truncated.iloc[index,0]
+    return(row)
+
+def textsent(text):
+    #get text sentiment of an article text, somehow
+    return(sid.polarity_scores(text))
 
 count = {}
 n =  0
@@ -15,27 +25,22 @@ current_docname = None
 last_sentiment = None
 topic_weights = {}
 
-
 #now that text sent model has been trained, iterate through news articles
 with open('/Users/andyliu/Downloads/doc-topics.csv', newline='') as csvfile:
-<<<<<<< HEAD
-=======
-    count = {}
-    total_sentiments = {}
-    topic_counts = {}
->>>>>>> parent of 36ef54e... asdfasfffew
     reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     first_row = next(reader)
 
     for row in reader:
-        docname = row[0]
+        if n > 1000:
+            break
+        docname = row[0].split(",")[0]
 
         if docname==current_docname:
             #still on the same doc, just different topics
             try:
-                topic_weights[int(row[1])] += int(row[2])
-            except ValueError:
-                topic_weights[int(row[1])] = int(row[2])
+                topic_weights[float(row[0].split(",")[1])] += float(row[0].split(",")[2])
+            except KeyError:
+                topic_weights[float(row[0].split(",")[1])] = float(row[0].split(",")[2])
         else:
             #new doc!
 
@@ -46,43 +51,27 @@ with open('/Users/andyliu/Downloads/doc-topics.csv', newline='') as csvfile:
                 except KeyError:
                     topic_counts[topic] = topic_weights[topic]
                 
-                topic_sentiment = topic_weights[topic] * last_sentiment
+                topic_sentiment = topic_weights[topic] * last_sentiment['pos']
                 try:
                     total_sentiments[topic] +=  topic_sentiment
                 except KeyError:
                     total_sentiments[topic] = topic_sentiment
 
-        
-
             #reset variables
             topic_weights = {}
-            current_docname = row[0]
+            current_docname = row[0].split(",")[0]
             
             #calculate sentiment of new thing
-            text_index = row[0].split(":")[-1]
-            text = getText(text_index)
+            text_index = str(row[0].split(":")[-1])[0]
+            text = getText(int(text_index))
             last_sentiment = textsent(text)
+            print(str(n) + ": " + str(last_sentiment))
+            n+=1
 
     #write total_sentiment divided by total_weight (topic_counts)
-    output = open("output.txt","a")
-
+with open("/Users/andyliu/Downloads/output.txt","a") as output:
     for key in total_sentiments.keys():
         value = total_sentiments[key]/topic_counts[key]
-<<<<<<< HEAD
         string = str(key) + "," + str(value) + "\n"
+        print(string)
         output.write(string)
-=======
-        string = key + "," + value + "\n"
-        output.write(string)
-
-    output.close()
-
-def getText(index):
-    #get the article text from the other spreadsheet (from the index)
-    row = " ".join(truncated.iloc[index])
-    return(row)
-
-def textsent(text):
-    #get text sentiment of an article text, somehow
-    return(sid.polarity_scores(text))
->>>>>>> parent of 36ef54e... asdfasfffew
